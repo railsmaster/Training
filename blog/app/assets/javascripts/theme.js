255e77c27e4984c32411d4d0ddbe7add3237f783
Ajay Raj Srivastava <ajayisready@gmail.com>
2017-06-07 14:30:52 +0530

    Initial commit

---
+++ blog/app/assets/javascripts/theme.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require jquery/jquery.min.js
+//= require bootstrap/js/bootstrap.min.js
+//= require js/jqBootstrapValidation.js
+//= require js/contact_me.js
+//= require js/clean-blog.min.js
